6e6edabd822bf157f73d8c80384838a50a08e4a2
kindywu <kindywu@outlook.com>
2024-06-03 13:44:43 +0800

    commit

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,5 +1,13 @@
 // cargo expand --example demo
 #[tokio::main]
 async fn main() {
-    println!("tokio main")
+    let mut data = vec![1, 2, 3];
+
+    let last = data.last();
+
+    // data.push(4);
+
+    println!("last:{last:?}");
+
+    data.push(4);
 }
