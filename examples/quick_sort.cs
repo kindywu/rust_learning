8350c21b700de08722583fce3d459d212a8da58b
kindywu <kindywu@outlook.com>
2024-06-19 14:17:41 +0800

    commit

--- examples/quick_sort.cs
+++ examples/quick_sort.cs
@@ -1,13 +1,13 @@
 int[] arr = { 34, 7, 23, 32, 5, 62, 1, 31, 32, 5 };
-Console.WriteLine("Original array: " + String.Join(", ", arr));
+Console.WriteLine("{0,-18} {1}", "Original array: ", String.Join(", ", arr));
 
 var segment = new ArraySegment<int>(arr);
 
 QuickSort(segment);
 
-Console.WriteLine("Sorted array: " + String.Join(", ", arr));
+Console.WriteLine("{0,-18} {1}", "Sorted array: ", String.Join(", ", arr));
 
-void QuickSort(ArraySegment<int> segment)
+void QuickSort<T>(ArraySegment<T> segment) where T : IComparable<T>
 {
     if (segment.Count > 1)
     {
@@ -23,7 +23,7 @@ void QuickSort(ArraySegment<int> segment)
     }
 }
 
-int Partition(ArraySegment<int> segment)
+int Partition<T>(ArraySegment<T> segment) where T : IComparable<T>
 {
     var pivotIndex = segment.Count / 2;
     var lastIndex = segment.Count - 1;
@@ -33,7 +33,7 @@ int Partition(ArraySegment<int> segment)
     var storeIndex = 0;
     for (var index = 0; index < lastIndex; index++)
     {
-        if (segment[index] < segment[lastIndex])
+        if (segment[index].CompareTo(segment[lastIndex]) < 0)
         {
             Swap(segment, index, storeIndex);
             storeIndex++;
@@ -45,7 +45,7 @@ int Partition(ArraySegment<int> segment)
     return storeIndex;
 }
 
-void Swap(ArraySegment<int> segment, int i, int j)
+void Swap<T>(ArraySegment<T> segment, int i, int j)
 {
     var tmp = segment[i];
     segment[i] = segment[j];
