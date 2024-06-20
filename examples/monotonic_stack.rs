320fb92ad35ba58bc183be9ee88d5df0b81fdc97
kindywu <kindywu@outlook.com>
2024-06-20 22:46:07 +0800

    commit

--- examples/monotonic_stack.rs
+++ examples/monotonic_stack.rs
@@ -1,4 +1,4 @@
-fn next_greater_elements(nums: &[i32], compare: fn(x: i32, y: i32) -> bool) -> Vec<i32> {
+fn monotonic_stack(nums: &[i32], compare: fn(x: i32, y: i32) -> bool) -> Vec<i32> {
     let mut stack = Vec::new();
     let mut result = vec![-1; nums.len()]; // 初始化结果数组，所有元素为-1
 
@@ -22,26 +22,30 @@ fn next_greater_elements(nums: &[i32], compare: fn(x: i32, y: i32) -> bool) -> V
     result
 }
 
-/*
-    source := []int{2, 1, 2, 4, 3}
-    want := []int{4, 2, 4, -1, -1}
-*/
 fn compare_bigger(x: i32, y: i32) -> bool {
     x < y
 }
 
-/*
-    source := []int{2, 1, 2, 4, 3}
-    want := []int{1, -1, -1, 3, -1}
-*/
+#[allow(dead_code)]
 fn compare_smaller(x: i32, y: i32) -> bool {
     x > y
 }
 
 fn main() {
-    let nums = vec![2, 1, 2, 4, 3];
-    let next_greater = next_greater_elements(&nums, compare_bigger);
-    println!("Next greater elements: {:?}", next_greater);
-    let next_greater = next_greater_elements(&nums, compare_smaller);
-    println!("Next greater elements: {:?}", next_greater);
+    // 当天的股票曲线，可能的最高收入是多少？
+    let nums = vec![30, 50, 90, 60, 125, 110];
+    println!("{:20}: {:?}", "elements", nums);
+    let next_greater = monotonic_stack(&nums, compare_bigger);
+    println!("{:20}: {:?}", "next elements", next_greater);
+
+    let difference: Vec<i32> = next_greater
+        .iter()
+        .zip(nums.iter())
+        .map(|(a, b)| a - b)
+        .collect();
+    println!("{:20}: {:?}", "difference elements", difference);
+    println!("{:20}: {:?}", "max value", difference.iter().max().unwrap());
+
+    // let next_smaller = monotonic_stack(&nums, compare_smaller);
+    // println!("Next smaller elements: {:?}", next_smaller);
 }
